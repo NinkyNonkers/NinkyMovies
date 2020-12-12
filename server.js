7dd2fc4fba8a5d2b1f19421c58dbe3916dd7a353
archie426 <archiestride42@gmail.com>
2020-12-12 22:22:34 +0000

    Revert "Default 3"
    This reverts commit b411f8416884fb89ec9a5e350f49fa588fada746.

--- server.js
+++ server.js
@@ -386,7 +386,7 @@ io.sockets.on('connection', function(socket) {
             var currTime = data.time
             var state = data.state
             var videoId = data.videoId
-            var playerId = 3
+            var playerId = room.currPlayer
             // var videoId = io.sockets.adapter.rooms['room-'+roomnum].currVideo
             io.sockets.in("room-" + roomnum).emit('syncVideoClient', {
                 time: currTime,
@@ -718,7 +718,7 @@ io.sockets.on('connection', function(socket) {
     // Change video player
     socket.on('change single player', function(data) {
         if (io.sockets.adapter.rooms['room-' + socket.roomnum] !== undefined) {
-            var playerId = 3
+            var playerId = data.playerId
 
             switch (playerId) {
                 case 0:
@@ -949,7 +949,7 @@ io.sockets.on('connection', function(socket) {
     function updateQueueVideos() {
         if (io.sockets.adapter.rooms['room-' + socket.roomnum] !== undefined) {
             var vidlist = io.sockets.adapter.rooms['room-' + socket.roomnum].queue
-            var currPlayer = 3
+            var currPlayer = io.sockets.adapter.rooms['room-' + socket.roomnum].currPlayer
             io.sockets.in("room-" + socket.roomnum).emit('get vidlist', {
                 vidlist: vidlist,
                 currPlayer: currPlayer,
