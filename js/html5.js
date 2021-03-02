161d238cd84876aced8c14a29593dadbba2ef0fb
archie426 <archiestride42@gmail.com>
2021-03-02 08:58:21 +0000

    Begin work on different video modes

--- js/html5.js
+++ js/html5.js
@@ -12,6 +12,14 @@
 
 var media = document.querySelector('video');
 
+let currentMode = 1;
+
+let videoModes = {
+    1: null,
+    2: null,
+    3: null
+};
+
 // Event listeners
 media.addEventListener("play", function(e) {
     if (host) {
@@ -41,8 +49,38 @@ function html5Play() {
     }
 }
 
+function changeMode() {
+    if (currentMode >= 3)
+        currentMode = 1;
+    else
+        currentMode++;
+
+    changeVideoMode(currentMode);
+}
+
+function changeVideoMode(newIndex) {
+    if (newIndex > videoModes.length - 1)
+        return;
+    media.src = videoModes[newIndex];
+}
+
 // Load video
 function htmlLoadVideo(videoId) {
-    console.log("changing video to: " + videoId)
+    console.log("changing video to: " + videoId);
+    if (isFolder(videoId)) {
+        console.log("Video is a directory, going to prepare individual files!");
+        videoModes[1] = videoId + "1.mp4";
+        videoModes[2] = videoId + "2.mp4";
+        videoModes[3] = videoId + "3.mp4";
+        media.src = videoModes[currentMode];
+        const switchModeButton = document.getElementById("switchmode");
+        if (switchModeButton !== undefined && switchModeButton != null)
+            switchModeButton.style.display = "block";
+        return;
+    }
     media.src = videoId
+}
+
+function isFolder(videoId) {
+    return !videoId.contains(".mp4") && videoId[videoId.length - 1] === '/';
 }
