964e98a9d71275e32b29d2b3d170c62d489aa07a
archie426 <archiestride42@gmail.com>
2021-03-08 12:20:59 +0000

    I DID WOOPS LOL

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io');
+const io = require('socket.io').listen(server);
 const compression = require('compression')
 
 users = [];
